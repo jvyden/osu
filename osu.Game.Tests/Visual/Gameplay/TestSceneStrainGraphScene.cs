47869c2bce126093150442dfea14fcec7080afe6
jvyden <jvyden@jvyden.xyz>
2025-04-21 05:10:45 -0400

    Improve display of strain graph

--- osu.Game.Tests/Visual/Gameplay/TestSceneStrainGraphScene.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneStrainGraphScene.cs
@@ -6,47 +6,40 @@ using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Testing;
-using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Rulesets.Osu.Difficulty;
 using osu.Game.Tests.Beatmaps;
-using osuTK;
 
 namespace osu.Game.Tests.Visual.Gameplay
 {
     [TestFixture]
     public partial class TestSceneStrainGraphScene : OsuTestScene
     {
-        private BarGraph graph = null!;
         private Ruleset ruleset = null!;
         private OsuDifficultyCalculator calculator = null!;
         private Dictionary<StrainSkill, List<double>> strainGraphs = null!;
+        private FillFlowContainer container = null!;
 
         [SetUpSteps]
         public void SetUpSteps()
         {
-            AddStep("create graph", () => Child = new Container
+            AddStep("create graph", () => Child = new OsuScrollContainer()
             {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(300, 150),
-                Children = new Drawable[]
+                Child = container = new FillFlowContainer()
                 {
-                    new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = OsuColour.Gray(0.2f)
-                    },
-                    graph = new BarGraph
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                    }
-                }
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    // Size = new Vector2(300, 150),
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                },
+                RelativeSizeAxes = Axes.Both,
             });
             AddStep("create difficulty calculator", () =>
             {
@@ -63,10 +56,27 @@ namespace osu.Game.Tests.Visual.Gameplay
                 strainGraphs = calculator.CalculateStrain();
             });
             AddAssert("has graphs", () => strainGraphs.Count != 0);
-            AddAssert("any graph has values", () => strainGraphs.Sum(g => g.Value.Count) != 0);
-            AddStep("add values to graph", () =>
+            AddAssert("any graph has value", () => strainGraphs.Sum(g => g.Value.Count) != 0);
+            AddStep("add graphs", () =>
             {
-                graph.Values = strainGraphs.First().Value.Select(v => (float)v);
+                foreach ((StrainSkill skill, List<double> list) in strainGraphs)
+                {
+                    LineGraph graph = new LineGraph
+                    {
+                        RelativeSizeAxes = Axes.X,
+                        Height = 64,
+                    };
+
+                    container.Add(new OsuSpriteText
+                    {
+                        Text = skill.GetType().Name,
+                        // RelativeSizeAxes = Axes.Both,
+                    });
+
+                    container.Add(graph);
+
+                    graph.Values = list.Select(v => (float)v);
+                }
             });
         }
     }
